a4862e75b6e00ea61c5fe069f1765f69c9d29c33
Lukas Tauch <lukastauch12012002@gmail.com>
2021-09-29 17:09:15 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
