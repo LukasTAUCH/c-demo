6a4d719e4fd04bc94d0be48654419b236c92a04f
Lukas Tauch <lukastauch12012002@gmail.com>
2021-09-29 17:10:10 +0100

    version 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace c_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! version 2");
         }
     }
 }
